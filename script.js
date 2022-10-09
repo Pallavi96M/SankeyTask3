b09702daf0894647a21cd7ddc2c8ca203d9edb74
Pallavi96M <114362551+Pallavi96M@users.noreply.github.com>
2022-10-09 19:37:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,93 +1,79 @@
-var selectedRow = null;
+var selectedRow = null
 
-function onFormSubmit(){
-    event.preventDefault();
-    var formData = readFormData();
-    if(selectedRow === null){
-        insertNewProduct(formData);
+function onFormSubmit() {
+    if (validate()) {
+        var formData = readFormData();
+        if (selectedRow == null)
+            insertNewRecord(formData);
+        else
+            updateRecord(formData);
+        resetForm();
     }
-    else
-    {
-        updateRecord(formData);
-    }
-    
-    resetForm();
 }
-    
 
-
-
-//retrieve the data
-function readFormData(){
+function readFormData() {
     var formData = {};
-    formData["id"] = document.getElementById("id").value;
-    formData["name"] = document.getElementById("name").value;
-    formData["gender"] = document.getElementById("gender").value;
-    formData["age"] = document.getElementById("age").value;
+    formData["fullName"] = document.getElementById("fullName").value;
+    formData["empId"] = document.getElementById("empId").value;
+    formData["salary"] = document.getElementById("salary").value;
+    formData["Age"] = document.getElementById("Age").value;
     return formData;
 }
 
-//Insert data
-function insertNewProduct(data){
-    var table = document.getElementById("storeList").getElementsByTagName("tbody")[0];
+function insertNewRecord(data) {
+    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
     var newRow = table.insertRow(table.length);
-    var cell1 = newRow.insertCell(0);
-        cell1.innerHTML = data.id;
-    var cell2 = newRow.insertCell(1);
-        cell2.innerHTML = data.name;
-    var cell3 = newRow.insertCell(2);
-        cell3.innerHTML = data.gender;
-    var cell4 = newRow.insertCell(3);
-        cell4.innerHTML = data.age;
-    var cell5 = newRow.insertCell(4);
-        cell5.innerHTML = `<button onClick='onEdit(this)'>Edit</button>  <button onClick='onDelete(this)'>Delete</button>`
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.fullName;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.empId;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.salary;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.Age;
+    cell4 = newRow.insertCell(4);
+    cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+                       <a onClick="onDelete(this)">Delete</a>`;
 }
 
-//Edit the data
-function onEdit(td){
+function resetForm() {
+    document.getElementById("fullName").value = "";
+    document.getElementById("empId").value = "";
+    document.getElementById("salary").value = "";
+    document.getElementById("Age").value = "";
+    selectedRow = null;
+}
+
+function onEdit(td) {
     selectedRow = td.parentElement.parentElement;
-    document.getElementById('id').value = selectedRow.cells[0].innerHTML;
-    document.getElementById('name').value = selectedRow.cells[1].innerHTML;
-    document.getElementById('gender').value = selectedRow.cells[2].innerHTML;
-    document.getElementById('age').value = selectedRow.cells[3].innerHTML;
+    document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("empId").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("salary").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("Age").value = selectedRow.cells[3].innerHTML;
+}
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.fullName;
+    selectedRow.cells[1].innerHTML = formData.empId;
+    selectedRow.cells[2].innerHTML = formData.salary;
+    selectedRow.cells[3].innerHTML = formData.Age;
 }
 
-function updateRecord(formData){
-    selectedRow.cells[0].innerHTML = formData.id;
-    selectedRow.cells[1].innerHTML = formData.name;
-    selectedRow.cells[2].innerHTML = formData.gender;
-    selectedRow.cells[3].innerHTML = formData.age;
-}
-
-//Delete the data
-function onDelete(td){
-    if(confirm('Do you want to delete this record?')){
+function onDelete(td) {
+    if (confirm('Are you sure to delete this record ?')) {
         row = td.parentElement.parentElement;
-        document.getElementById('storeList').deleteRow(row.rowIndex);
+        document.getElementById("employeeList").deleteRow(row.rowIndex);
+        resetForm();
     }
-    resetForm();
 }
-
-
-//Reset the data
-function resetForm(){
-    document.getElementById('id').value = '';
-    document.getElementById('name').value = '';
-    document.getElementById('gender').value = '';
-    document.getElementById('age').value = '';
-}
-
-function my_function(){
-    if(document.getElementById('gender_male').checked)
-    {
-        var gender="male"
-        document.getElementById("my_myoutput").innerHTML=("your "+gender);
+function validate() {
+    isValid = true;
+    if (document.getElementById("fullName").value == "") {
+        isValid = false;
+        document.getElementById("fullNameValidationError").classList.remove("hide");
+    } else {
+        isValid = true;
+        if (!document.getElementById("fullNameValidationError").classList.contains("hide"))
+            document.getElementById("fullNameValidationError").classList.add("hide");
     }
-    else{
-        if(document.getElementById('gender_female').checked)
-    {
-        var gender="female"
-        document.getElementById("my_myoutput").innerHTML=("your "+gender);
-    }
-    }
+    return isValid;
 }
